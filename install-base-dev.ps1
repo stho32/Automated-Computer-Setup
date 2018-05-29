d23b304b0ad8a89a9e034f342eb67d616301619d
stho32 <sthoffmann@onlinehome.de>
2018-05-29 21:26:36 +0200

    Update install-base-dev.ps1

--- install-base-dev.ps1
+++ install-base-dev.ps1
@@ -1,3 +1,5 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
+chocolatey feature enable -n allowGlobalConfirmation
+
 choco install git visualstudiocode powershell-core firefox
