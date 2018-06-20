c963f9e7243e5cacab2f5cfc281f4bc730a11c33
Stefan Hoffmann <stho32github@gmail.com>
2018-06-20 22:02:17 +0200

    Add optional installation of sql server management studio

--- install-base-dev.ps1
+++ install-base-dev.ps1
@@ -6,9 +6,15 @@
     + vscode
     + powershell-core
     + firefox
+    + sql-server-management-studio 
 
 #>
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 choco install -y git visualstudiocode powershell-core firefox 
+
+$doIt = Read-Host "Do you wish to install SQL Server and Tools? (y/n)"
+if ($doIt -eq "y") {
+    choco install -y sql-server-management-studio 
+}
