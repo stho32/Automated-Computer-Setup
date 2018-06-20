d3ac33adc1b81329b1efc10002eee6dda1172b07
Stefan Hoffmann <stho32github@gmail.com>
2018-06-20 21:51:11 +0200

    Added comment

--- install-base-dev.ps1
+++ install-base-dev.ps1
@@ -1,3 +1,14 @@
+<#
+    Installation of a basic dev computer:
+
+    - set up chocolatey
+    + git 
+    + vscode
+    + powershell-core
+    + firefox
+
+#>
+
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 choco install -y git visualstudiocode powershell-core firefox 
